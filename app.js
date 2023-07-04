9fdde2d79e13d0194519a916d300a960601607d1
Ramya Punukollu <ramya.punukollu8@gmail.com>
2023-07-04 13:03:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// Function to save the attendance data
+function saveAttendance(event) {
+  event.preventDefault(); // Prevent form submission
+
+    const modeSelect = document.getElementById('mode');
+    const datetimeInput = document.getElementById('datetime');
+
+    const mode = modeSelect.value;
+    const datetime = datetimeInput.value;
+
+    const attendanceData = {
+    mode,
+    datetime
+    };
+
+  // Save attendance data to local storage or perform other operations as needed
+  // Example: Save data to an array
+    attendance.push(attendanceData);
+    console.log('attendance');
+
+  // Clear the form inputs
+  modeSelect.value = 'entry'; // Set the default value for mode
+    datetimeInput.value = '';
+
+  // Update the attendance list
+    updateAttendanceList();
+
+    console.log('Attendance saved:', attendanceData);
+}
+
+// Function to update the attendance list
+function updateAttendanceList() {
+    const attendanceList = document.getElementById('attendanceList');
+
+  // Clear the list
+    attendanceList.innerHTML = '';
+
+  // Create a new list item for each attendance entry
+    attendance.forEach(function(data) {
+    const listItem = document.createElement('div');
+    listItem.textContent = `Mode: ${data.mode}, Datetime: ${data.datetime}`;
+    attendanceList.appendChild(listItem);
+    });
+}
+
+// Array to store attendance data
+const attendance = [];
+
+// Add event listener to the form submit event
+const attendanceForm = document.getElementById('attendanceForm');
+attendanceForm.addEventListener('submit', saveAttendance);
